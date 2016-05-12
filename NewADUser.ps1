9c664da91f636469f9c0155d3b122fedee52e2bb
Bud Calabrese <budcalabrese@gmail.com>
2016-05-12 12:17:41 -0500

    NewADUser
    This creates a new AD user and copies permissions from another account.

---
+++ NewADUser.ps1
@@ -0,0 +1,33 @@
+# Import the Active Directory module and continue if already installed
+
+Import-Module ActiveDirectory
+
+# Gathering variables for new account creation
+
+$FirstName = Read-Host "Enter First Name"
+$LastName = Read-Host "Enter Last Name"
+$AccountName = Read-Host "Enter account name (Example jsmith)"
+$Title = Read-Host "Enter Job Title"
+$CopyUser = Read-Host "Enter a user to copy permissions from (Example jsmith)"
+$Password = Read-Host "Enter password for new account" -AsSecureString
+
+#Creates a new Active Directory user account using the variables above
+
+New-ADUser `
+ -Name ("$FirstName $LastName") `
+ -SamAccountName  ("$AccountName") `
+ -GivenName "$FirstName" `
+ -Surname "$LastName" `
+ -DisplayName ("$FirstName $LastName") `
+ -AccountPassword $Password `
+ -UserPrincipalName "$AccountName@V-Blog.local" `
+ -EmailAddress "$AccountName@V-Blog.local" `
+ -Title "$Title" `
+ -Enabled $true
+
+# Copies the group memberships of one user to another
+
+Get-ADUser -Identity $CopyUser -Properties memberof |
+Select-Object -ExpandProperty memberof |
+Add-ADGroupMember -Members $AccountName -PassThru |
+Select-Object -Property SamAccountName
