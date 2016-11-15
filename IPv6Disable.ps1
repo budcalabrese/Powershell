57f47abb2a477121fabf118ce3d23e24f5c128b7
Bud Calabrese <budcalabrese@gmail.com>
2016-11-15 17:36:58 -0600

    IPv6 Disable
    Used to disable IPv6

---
+++ IPv6Disable.ps1
@@ -0,0 +1,23 @@
+﻿#### Disabling IPv6 and rebooting the machine ####
+#### To remotely disable IPv6 winRM will have to be enabled ####
+
+#Credentials used to connect to remote computer
+$adminUser = "DOMAIN\USERNAME"
+$adminPwd = "PASSWORD"
+$compName = "REMOTECOMPUTERNAME"
+
+ 
+$secPwd = ConvertTo-SecureString $adminPwd -AsPlainText -Force
+$remoteCreds = New-Object System.Management.Automation.PSCredential ($adminUser, $secPwd)
+$ServerSession = New-PSSession -ComputerName $compName -Authentication CredSSP -Credential $remoteCreds
+ 
+Invoke-Command -Session $ServerSession -ScriptBlock 
+{
+	# Disable IPv6
+	$regPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters"
+	New-ItemProperty -Path $regPath -Name "DisabledComponents" -Value "0xFFFFFFFF" -PropertyType "DWORD" | Out-Null
+	
+}
+
+# Restarting computer so change will take affect
+Restart-Computer -ComputerName $env:computername -Force
